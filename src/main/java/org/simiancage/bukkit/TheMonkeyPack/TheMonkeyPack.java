f553e160cd22e3cc712a9392bb2f84acceb2a601
Don Redhorse <dredhorse@breiden.net>
2011-12-07 23:51:27 +0100

    First commit

---
+++ src/main/java/org/simiancage/bukkit/TheMonkeyPack/TheMonkeyPack.java
@@ -0,0 +1,28 @@
+package org.simiancage.bukkit.TheMonkeyPack;
+
+/**
+ *
+ * PluginName: TheMonkeyPack
+ * Class: TheMonkeyPack
+ * User: DonRedhorse
+ * Date: 07.12.11
+ * Time: 23:46
+ *
+ */
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheMonkeyPack extends JavaPlugin {
+
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
+
