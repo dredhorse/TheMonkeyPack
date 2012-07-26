933ad0b82c2c9aef85fa0d50201166405805bbe4
dredhorse <dredhorse@breiden.net>
2012-07-26 21:30:31 +0200

    fixed some issues with command handling (hopefully), fixes #11 v 0.16

--- src/main/java/org/simiancage/bukkit/TheMonkeyPack/listeners/PlayerChatListener.java
+++ src/main/java/org/simiancage/bukkit/TheMonkeyPack/listeners/PlayerChatListener.java
@@ -17,42 +17,46 @@ import org.simiancage.bukkit.TheMonkeyPack.loging.MainLogger;
  */
 
 public class PlayerChatListener implements Listener {
-	MainConfig mainConfig;
-	MainLogger mainLogger;
-	TheMonkeyPack main;
+    MainConfig mainConfig;
+    MainLogger mainLogger;
+    TheMonkeyPack main;
 
 
-	public PlayerChatListener(TheMonkeyPack instance) {
-		main = instance;
-		mainConfig = main.getMainConfig();
-		mainLogger = main.getMainLogger();
-		mainLogger.debug("PlayerChat Listener for commands registered");
-	}
+    public PlayerChatListener(TheMonkeyPack instance) {
+        main = instance;
+        mainConfig = main.getMainConfig();
+        mainLogger = main.getMainLogger();
+        mainLogger.debug("PlayerChat Listener for commands registered");
+    }
 
 
-	@EventHandler
-	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		String[] args = event.getMessage().split(" ");
-		String command = args[0].substring(1);
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        String[] args = event.getMessage().split(" ");
+        if (args.length < 1) {
+            mainLogger.debug("There is something weird with the args, they are empty");
+            return;
+        }
+        String command = args[0].substring(1);
 
-		if (!isValidCommand(command)) {
-			mainLogger.debug("not a valid TheMonkeyPack command");
-			return;
-		}
-		event.setCancelled(true);
-		mainLogger.debug("Executing command now: ", command);
-		Player player = event.getPlayer();
-		String commandArgs[] = new String[args.length - 1];
-		if (args.length > 1) {
-			for (int i = 1; i < args.length; i++) {
-				commandArgs[i - 1] = args[i];
-			}
-		}
-		main.getPlayerCommand(command).onPlayerCommand(player, commandArgs);
-	}
+        if (!isValidCommand(command)) {
+            mainLogger.debug("not a valid TheMonkeyPack command");
+            return;
+        }
+        event.setCancelled(true);
+        mainLogger.debug("Executing command now: ", command);
+        Player player = event.getPlayer();
+        String commandArgs[] = new String[args.length - 1];
+        if (args.length > 1) {
+            for (int i = 1; i < args.length; i++) {
+                commandArgs[i - 1] = args[i];
+            }
+        }
+        main.getPlayerCommand(command).onPlayerCommand(player, commandArgs);
+    }
 
-	public boolean isValidCommand(String command) {
-		return main.isValidPlayerCommand(command);
-	}
+    public boolean isValidCommand(String command) {
+        return main.isValidPlayerCommand(command);
+    }
 }
 
